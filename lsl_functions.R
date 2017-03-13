f81bb782318871c3b2653e5acbb8430755aa8202
loveakai <loveakai@gmail.com>
2017-03-13 15:40:30 +0800

    fix import

--- lsl_functions.R
+++ lsl_functions.R
@@ -1,3 +1,45 @@
+import <-
+  function(raw_obs,var_subset,var_group,obs_subset,obs_weight,raw_cov,raw_mean,obs_size) {
+    if (missing(raw_obs)) {
+      output<-list(raw_obs = raw_obs, raw_mean = raw_mean)
+      if (exists(obs_size)) {attr(output,"obs_size")<-obs_size}
+    }  else {
+      if (missing(obs_subset)) {
+        obs_subset <- 1:nrow(raw_obs)
+      }
+      if (missing(var_group)) {
+        if (missing(var_subset)) {
+          var_subset <- 1:ncol(raw_obs)
+        }
+        raw_obs %<>% .[obs_subset, ] %>% cbind(group = 1)
+        var_group <-ncol(raw_obs)
+      } else {
+        if (is.character(var_group)) {
+          var_group <- which(colnames(raw_obs)%in%(var_group))
+        }
+        if (missing(var_subset)) {
+          var_subset <- (1:ncol(raw_obs)) %>% .[!. %in% var_group]
+        }
+        raw_obs %<>% .[obs_subset, c(var_subset, var_group)]
+      }
+      output<-list(
+        raw_obs = raw_obs,
+        raw_cov = split(raw_obs, raw_obs[,ncol(raw_obs)]) %>% lapply(function(x) {
+          cov(x[, -ncol(x)])
+        }),
+        raw_mean = split(raw_obs, raw_obs[,ncol(raw_obs)]) %>% lapply(function(x) {
+          apply(x[, -ncol(x)], 2, mean)
+        })
+      )
+      attr(output,"obs_size")  <- plyr::count(raw_obs[,var_group]) %>% .[,2]
+      
+    }
+    attr(output,"var_group") <- output$raw_cov %>% length
+    return(output)
+  }
+
+
+
 betagen   <- function(lambda){
   
   beta_p                          <- matrix(0, ncol = n_eta, nrow = n_eta)
@@ -319,44 +361,7 @@ dml_cal   <- function(sigma=sigma,e_v=e_v,ini=ini,G_eta=G_eta,n_groups=n_groups,
   return(dml)
 }
 
-import <-
-  function(raw_obs,var_subset,var_group,obs_subset,obs_weight,raw_cov,raw_mean,obs_size) {
-    if (missing(raw_obs)) {
-      output<-list(raw_obs = raw_obs, raw_mean = raw_mean)
-      if (exists(obs_size)) {attr(output,"obs_size")<-obs_size}
-    }  else {
-      if (missing(obs_subset)) {
-        obs_subset <- 1:nrow(raw_obs)
-      }
-      if (missing(var_group)) {
-        if (missing(var_subset)) {
-          var_subset <- 1:ncol(raw_obs)
-        }
-        raw_obs %<>% .[obs_subset, ] %>% cbind(group = 1)
-        var_group <-ncol(raw_obs)
-      } else {
-        if (is.character(var_group)) {
-          var_group <- which(colnames(raw_obs)%in%(var_group))
-        }
-        if (missing(var_subset)) {
-          var_subset <- (1:ncol(raw_obs)) %>% .[!. %in% var_group]
-        }
-        raw_obs %<>% .[obs_subset, c(var_subset, var_group)]
-      }
-    output<-list(ㄋ
-      raw_obs = raw_obs,
-      raw_cov = split(raw_obs, raw_obs[, var_group]) %>% lapply(function(x) {
-        cov(x[, -ncol(x)])
-      }),
-      raw_mean = split(raw_obs, raw_obs[, var_group]) %>% lapply(function(x) {
-        apply(x[, -ncol(x)], 2, mean)
-      })
-    )
-    attr(output,"obs_size")<- plyr::count(raw_obs[,var_group]) %>% .[,2]
-    attr(output,"var_group")<-output$raw_cov %>% length
-    }
-    return(output)
-  }
+
   
 getpar    <-function(pattern,value,v_label,f_label,mat_label){
  mapply(function(val,p,v,nm) {
